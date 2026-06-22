55d85387921a05a8c4a38580b7ee78c6ea99a81c
ring128 <ring128@users.noreply.github.com>
2026-06-22 23:52:42 +0900

    Add ABC452 A

---
+++ ABC/A/452.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int M, D;
+    cin >> M >> D;
+
+    pair<int, int> a = {M, D};
+
+    set<pair<int, int>> ok = {
+        {1, 7}, {3, 3}, {5, 5}, {7, 7}, {9, 9}
+    };
+
+    if(ok.contains(a)) cout << "Yes\n";
+    else cout << "No\n";
+}
