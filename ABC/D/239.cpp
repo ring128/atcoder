0516943ea5a127151577eca903c0674cb3f44207
ring128 <ring128@users.noreply.github.com>
2026-07-02 13:37:47 +0900

    update

--- ABC/D/239.cpp
+++ ABC/D/239.cpp
@@ -27,18 +27,34 @@ using Graph = vector<vector<int>>;
 #define all(v) (v).begin(), (v).end()
 #define lb lower_bound
 #define ub upper_bound
+template<class... T>
+void ci(T&... x){
+    (cin >> ... >> x);
+}
+bool isP(long long N){
+    if(N < 2) return false;
+    for(long long i = 2; i <= N / i; i++){
+        if(N % i == 0) return false;
+    }
+    return true;
+}
 
 int main(){
     ios::sync_with_stdio(false);
-    int X, N; cin >> X >> N;
-    vector<pair<int, bool>> W(N);
-    for(auto& [p, f] : W) cin >> p;
-    int Q; cin >> Q;
-    int we = X;
-    fo(i, 0, Q){
-        int P; cin >> P; --P;
-        we += (W[P].second ? -W[P].first : W[P].first);
-        W[P].second = !W[P].second;
-        cout << we << '\n';
+    int A, B, C, D; ci(A, B, C, D);
+    fo(i, A, B+1){
+        bool tf = false;
+        fo(j, C, D+1){
+            if(isP(i+j)){
+                tf = false;
+                break;
+            }    
+            tf = true;
+        }
+        if(tf){
+            cout << "Takahashi" << '\n';
+            return 0;
+        }    
     }
+    cout << "Aoki" << '\n';
 }
