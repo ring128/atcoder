e88887a95f6141915817d3d78ab029f1940455a7
ring128 <ring128@users.noreply.github.com>
2026-07-10 15:35:59 +0900

    update

---
+++ ABC/B/424.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <tuple>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <functional>
+#include <cassert>
+using namespace std;
+using ll = long long;
+using Graph = vector<vector<int>>;
+#define rep(i, a, b) for(int i = (a); i < (b); i++) 
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define lb lower_bound
+#define ub upper_bound
+template<class... T>
+void ci(T&... x){
+    (cin >> ... >> x);
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    int N, M, K; ci(N, M, K);
+    map<int, int> m{};
+    vector<int> v;
+
+    rep(i, 0, K){
+        int A, B; ci(A, B);
+        m[A]++;
+        if(m[A] == M) v.pb(A);
+    }
+    
+    for(auto& p : v) cout << p << " ";
+}
