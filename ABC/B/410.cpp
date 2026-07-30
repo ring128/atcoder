7e757912d3fd5651d9cc6499d19b63c838964e98
ring128 <ring128@users.noreply.github.com>
2026-07-31 01:47:53 +0900

    update

---
+++ ABC/B/410.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <numeric>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <utility>
+#include <tuple>
+#include <array>
+#include <bitset>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+#include <functional>
+#include <cassert>
+using namespace std;
+using ll = long long;
+using i128 = __int128_t;
+#define rep(i, a, b) for(int i = (a); i < (b); i++) 
+#define pb push_back
+#define all(v) (v).begin(), (v).end()
+#define lb lower_bound
+#define ub upper_bound
+template<class... T>
+void ci(T&... x){
+    (cin >> ... >> x);
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+
+    int N, Q; ci(N, Q);
+    set<pair<int, int>> s;
+    vector<int> cnt(N);
+    rep(i, 0, N) s.insert({0, i});
+
+    rep(i, 0, Q){
+        int X; ci(X);
+        int n;
+        if(X > 0) n = X - 1;
+        else n = s.begin()->second;
+        s.erase({cnt[n], n});
+        cnt[n]++;
+        s.insert({cnt[n], n});
+        cout << n + 1 << " ";
+    }
+}
